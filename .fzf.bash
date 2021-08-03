ee12792fcf5494b23e2e85ba9ad3c017e3c93542
Jeffrey Hood <jeffrey.hood@gmail.com>
2021-08-03 16:31:09 -0400

    fzf

---
+++ .fzf.bash
@@ -0,0 +1,10 @@
+# Setup fzf
+# ---------
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "~/.config/fzf/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "~.config/fzf/key-bindings.bash"
